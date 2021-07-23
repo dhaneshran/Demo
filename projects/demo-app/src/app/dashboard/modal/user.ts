6481e9121478ece3c636f154e7bac3f3ca319d74
SwatiJoshi <swatijoshi1@kpmg.com>
2021-07-23 09:02:39 +0530

    validation added

---
+++ projects/demo-app/src/app/dashboard/modal/user.ts
@@ -0,0 +1,5 @@
+export class User {
+    firstName: string |undefined;
+    lastName: string|undefined;
+    phoneNumber: number|undefined;
+}
--- projects/demo-app/src/app/dashboard/pages/first-page/first-page.component.ts
+++ projects/demo-app/src/app/dashboard/pages/first-page/first-page.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../../modal/user';
 @Component({
   selector: 'app-first-page',
   templateUrl: './first-page.component.html',
   styleUrls: ['./first-page.component.css']
 })
 export class FirstPageComponent implements OnInit {
-
-  constructor() { }
+  user: User;
+  constructor() { 
+    this.user = new User();
+  }
 
   ngOnInit(): void {
+    //this.user.firstName = 'Dhanesh';
   }
 
 }
